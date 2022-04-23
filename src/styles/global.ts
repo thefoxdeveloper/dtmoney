f4b906b362dd7bd18e8de815a34da3387575faa0
Douglas Santos <88425982+foxdevfull@users.noreply.github.com>
2022-04-23 07:48:13 -0300

    atualizacao do google fonts

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -21,6 +21,14 @@ export const GlobalStyle = createGlobalStyle`
 body{
     background: var(--background-color);
     -webkit-font-smoothing: antialiased;
+
+}
+body, input,textarea, button{
+    font-family: 'Poppins', sans-serif;
+    font-weight: 400;
+}
+h1, h2, h3, h4, h5, h6,strong{
+    font-weight: 600;
 }
 html{ 
     @media(max-width:1080px){
