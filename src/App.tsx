ea8d909e96b586bb27d6cc45da9117d22eb5116c
Douglas Santos <88425982+foxdevfull@users.noreply.github.com>
2022-04-24 21:01:05 -0300

    modal as a component alone

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
 import { Dashboard } from "./components/dashboard";
+import Modal from "react-modal";
 import { Header } from "./components/header";
 import { GlobalStyle } from "./styles/global";
-
+import { useState } from "react";
+import { NewTransactionModal } from "./components/newTransactionModal";
+Modal.setAppElement("#root");
 export function App() {
+  const [isNewTransactionModalOpen, setisNewTransactionModalOpen] =
+    useState(false);
+  function handleOpenNewTransactionModal() {
+    setisNewTransactionModalOpen(true);
+  }
+  function handleCloseNewTransactionModal() {
+    setisNewTransactionModalOpen(false);
+  }
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
       <GlobalStyle />
     </>
   );
